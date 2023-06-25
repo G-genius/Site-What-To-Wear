34a3d3ab3074625ea6e1315ed95b6ffe10a10da2
G-genius <gamelob25@gmail.com>
2023-06-25 22:12:02 +0300

    Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,14 +6,24 @@ const Header = () => {
     return (
         <header>
             <div className="sidebar">
-                <img src={logo} alt="logo"/>
-                <ul>
-                    <li>Для кого</li>
-                    <li>Преимущества</li>
-                    <li>Как это работает</li>
-                </ul>
-                <button>Устновить приложение</button>
-                <span>EN</span>
+                <div className="left-side">
+                    <img src={logo} alt="logo"/>
+                    <a href="">What to wear</a>
+                </div>
+                <div className="center-side">
+                    <nav aria-label="Основные разделы на странице">
+                        <ul>
+                            <li>Для кого</li>
+                            <li>Преимущества</li>
+                            <li>Как это работает</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="right-side">
+                    <button>Устновить приложение</button>
+                    <span>EN</span>
+                </div>
+
             </div>
         </header>
     );
